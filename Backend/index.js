6a2199c3c778485df559dfaab633556752a015ef
David Jimenez <Jdceron2@misena.edu.co>
2018-02-12 15:11:47 -0500

    Se arreglo la conexion a la base de datos

--- Backend/index.js
+++ Backend/index.js
@@ -6,7 +6,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : 'angel',
+  password : 'J2d8c1j198',
   database : 'simuladorBancario'
 });
 
