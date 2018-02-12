c2674c434888c1007685b91f0835a6b0c50cdf07
David Jimenez <Jdceron2@misena.edu.co>
2018-02-12 17:43:26 -0500

    Hello

--- Backend/controllers/usuarios.controller.js
+++ Backend/controllers/usuarios.controller.js
@@ -3,25 +3,40 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'angel',
-  database : 'simuladorBancario'
+    host: 'localhost',
+    user: 'root',
+    password: 'J2d8c1j198',
+    database: 'simuladorBancario'
 });
 
-function getUsuarios (req, res) {
-  connection.query('SELECT * FROM Usuarios',  (error, results, fields) => {
-  if (error) throw error;
-  res.status(200).send(results);
-  });
+function getUsuarios(req, res) {
+    connection.query('SELECT * FROM Usuarios', (error, results, fields) => {
+        if (error) throw error;
+        res.status(200).send(results);
+    });
 }
 
-function createUser (req, res){
-  res.status(200).send(req.body);
-  let usuario =
+function createUser(req, res) {
+    let usuario = req.body;
+    connection.query("SELECT * FROM Usuarios WHERE email = ?", [req.body.email], (error, results, fields) => {
+        if (error) { throw error; } else {
+            if (results.length > 0) {
+                res.status(400).send({ message: 'Email' });
+            } else {
+                connection.query("INSERT INTO Usuarios SET ?", req.body, (error, results, fields) => {
+                    if (error) throw error;
+                    res.status(200).send({ message: 'Usuario creado correctamente' });
+                });
+            }
+        }
+    });
+}
+
+function findByEmail(email) {
+    res.status(200).send(req.body);
 }
 
 module.exports = {
-  getUsuarios,
-  createUser
+    getUsuarios,
+    createUser
 }
--- Backend/index.js
+++ Backend/index.js
@@ -7,29 +7,29 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'J2d8c1j198',
-  database : 'simuladorBancario'
+    host: 'localhost',
+    user: 'root',
+    password: 'J2d8c1j198',
+    database: 'simuladorBancario'
 });
 
 connection.connect(function(err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  }
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
 
-  console.log('connected as id ' + connection.threadId);
+    console.log('connected as id ' + connection.threadId);
 
-  app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
-  });
+    app.listen(3000, function() {
+        console.log('Example app listening on port 3000!');
+    });
 
 });
 
 app.get('/usuarios', usuariosControlller.getUsuarios);
 app.post('/usuarios', usuariosControlller.createUser);
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/', function(req, res) {
+    res.send('Hello World!');
 });
