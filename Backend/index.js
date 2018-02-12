4c262ced1ed84fce0ef43d6e4010e833b2386f77
yvmolina19 <yvmolina19@misena.edu.co>
2018-02-12 09:29:29 -0500

    Se reliza la conexion a la base de datos y una consulta de prueba

--- Backend/index.js
+++ Backend/index.js
@@ -7,19 +7,31 @@ var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : 'angel',
-  database : 'my_db'
+  database : 'simuladorBancario'
 });
 
-connection.connect();
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  console.log('connected as id ' + connection.threadId);
+
+  app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+  });
+
+});
+
+app.get('/ciudades', function (req, res) {
+  connection.query('SELECT * FROM CIudades',  (error, results, fields) => {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+  res.send(results);
+  });
+});
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
-});
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
 });
